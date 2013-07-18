45006ec75a6036a02055dc30bb8d4f09af9b542e
Omar Darwish <om.drwsh@gmail.com>
2013-07-17 19:51:49 -0500

    added more User tests

--- spec/test_user.rb
+++ spec/test_user.rb
@@ -1,4 +1,4 @@
-require_relative '../lib/pandora'
+require_relative '../lib/user'
 include Pandora
 
 describe User, '#new' do	
@@ -12,4 +12,32 @@ describe User, '#new' do
 			user.user.should eql 'om.drwsh'
 		end	
 	
+end
+
+describe User, '#get_stations' do
+
+	it "should return an array of Pandora::Station objects" do
+		user = User.new('om.drwsh')
+		stations = user.get_stations
+		stations.should have(9).stations #current number of statios for user om.drwsh as of 07/17/13
+	end
+	it "should return nil if the user has no satations" do
+		user = User.new('omarfdarwish') #this account has no stations
+		stations = user.get_stations
+		stations.should be_instance_of(nil)
+		
+	end
+	it "should return nil if the user does not exist" do
+		user = User.new('thisuserdoesnotexist')
+		stations = user.get_stations
+		stations.should be_instance_of(nil)
+	end
+		
+	it "should return nil if the user is private" do
+		user = User.new('vuhoaipham22')
+		stations = user.get_stations
+		stations.should be_instance_of(nil)
+	end
+		
+
 end
